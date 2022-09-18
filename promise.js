876bdfa006f6ed9306ef1fe49349f3d0b40ad071
Lanapostman <maulana4real@gmail.com>
2022-09-18 15:04:59 +0800

    modifikasi file promise.js

--- promise.js
+++ promise.js
@@ -1,8 +1,26 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+// const promiseOutput = null;
+const promiseOutput = async (params) => {
+  try {
+   const array1 = await promiseTheaterIXX();
+   const array2 = await promiseTheaterVGC();
+ 
+   const newArray = array1.concat(array2);
+ 
+   const count = newArray.filter((obj) => obj.hasil === params).length;
+   console.log(count);
+  }
+  catch (err) {
+   console.log(err);
+  }
+ }
 
-module.exports = {
+module.exports = {  
   promiseOutput,
 };
+
+
+
+
